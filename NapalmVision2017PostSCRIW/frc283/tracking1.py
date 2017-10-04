ef49ea9efa70859759036a3838a096d420631672
FRC-283-PROG-2 <frcprogramming283.2@gmail.com>
2017-10-04 13:54:32 -0400

    TBD

--- NapalmVision2017PostSCRIW/frc283/tracking1.py
+++ NapalmVision2017PostSCRIW/frc283/tracking1.py
@@ -1,7 +1,20 @@
 import cv2
 
-vid_path = ""
+vid_path = "C:\\Users\\FRC_STANDARD_USER\\Desktop\\Python Vision Code\\Python Vision Repository\\NapalmVision2017PostSCRIW\\test_video.mov"
 cap = cv2.VideoCapture(vid_path)
-#First, we convert to hue-saturation-lightness colorspace
-frame = cap.read();
-hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) #Converts to hsv color space
+print(cap.isOpened())
+
+while True :
+    good, img = cap.read()
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    img_h = img[:,:,0] #Image but ONLY hue values
+    img_h_copy = img_h
+    #Green is between 60 and 160
+    good, img_h = cv2.threshold(img_h, 60, 255, cv2.THRESH_BINARY) #Pixels above 82 become white
+    good2, img_h2 = cv2.threshold(img[:,:,0], 120, 255, cv2.THRESH_BINARY) #Pixels above 2nd number become white
+    img_f = cv2.bitwise_xor(img_h, img_h2) 
+    cv2.imshow("h", img_h)
+    cv2.imshow("h2", img_h2)
+    cv2.imshow("combined", img_f)
+    cv2.waitKey(0)
+cv2.destroyAllWindows()

efde1187607220b9b404ddb8bea084f996044e8b
FRC-283-PROG-2 <frcprogramming283.2@gmail.com>
2017-10-05 14:07:35 -0400

    Working CM Display
    Ben
    Program finds and displays combined COM for 2 largest contours

--- NapalmVision2017PostSCRIW/frc283/contours0.py
+++ NapalmVision2017PostSCRIW/frc283/contours0.py
@@ -3,7 +3,7 @@ import numpy
 
 path = "C:\\Users\\FRC_STANDARD_USER\Desktop\\Python Vision Code\\Python Vision Repository\\NapalmVision2017PostSCRIW\\testimagesmall.png"
 path2 = "C:\\Users\Benjamin\\Desktop\\Python\\Python Vision\\Python Vision Repository\\283-Vision\\NapalmVision2017PostSCRIW\\testimagesmall.png"
-img = cv2.imread(path2)
+img = cv2.imread(path)
 img_copy = img.copy()
 img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 GREEN_LOWER = numpy.array([70, 170, 150])
@@ -20,8 +20,19 @@ for i in cons:
     reverse.update({cv2.contourArea(i): index})
     index += 1
 areas = sorted(areas, key=float, reverse=True)
+moments = [cv2.moments(cons[reverse[areas[0]]]), cv2.moments(cons[reverse[areas[1]]])] #Moments of the two highest-area contours
 cv2.drawContours(img_copy, cons, reverse[areas[0]], (0, 0, 255), 3) 
 cv2.drawContours(img_copy, cons, reverse[areas[1]], (0, 0, 255), 3) 
+print(moments[0]['m10']/moments[0]['m00'])
+cc0 = (int(moments[0]['m10']/moments[0]['m00']), int(moments[0]['m01']/moments[0]['m00']))
+cc1 = (int(moments[1]['m10']/moments[1]['m00']), int(moments[1]['m01']/moments[1]['m00']))
+#print("Contour 0: cx: " + str(cc0[0]) + " cy: " + str(cc0[1]))
+#print("Contour 1: cx: " + str(cc1[0]) + " cy: " + str(cc1[1]))
+cv2.circle(img_copy, (cc0[0], cc0[1]), 12, (255, 0, 0), 3)
+cv2.circle(img_copy, (cc1[0], cc1[1]), 12, (255, 0, 0), 3)
+x_avg = int((cc0[0] + cc1[0])/2)
+y_avg = int((cc0[1] + cc1[1])/2) 
+cv2.circle(img_copy, (x_avg, y_avg), 14, (0, 255, 0), 4)
 cv2.imshow("the image", img_copy)
 cv2.waitKey(0)  
 cv2.destroyAllWindows()
--- NapalmVision2017PostSCRIW/frc283/contours1.py
+++ NapalmVision2017PostSCRIW/frc283/contours1.py
@@ -3,7 +3,7 @@ import numpy
 
 vid_path = "C:\\Users\\FRC_STANDARD_USER\\Desktop\\Python Vision Code\\Python Vision Repository\\NapalmVision2017PostSCRIW\\test_video.mov"
 vid_path2 = "C:\\Users\Benjamin\\Desktop\\Python\\Python Vision\\Python Vision Repository\\283-Vision\\NapalmVision2017PostSCRIW\\test_video.mov"
-cap = cv2.VideoCapture(vid_path2)
+cap = cv2.VideoCapture(0)
 GREEN_LOWER = numpy.array([50, 140, 50])
 GREEN_UPPER = numpy.array([140, 255, 255])
 while True :
@@ -22,6 +22,14 @@ while True :
     areas = sorted(areas, key=float, reverse=True)
     img = cv2.drawContours(img, dumby[1], reverse[areas[0]], (0, 0, 255), 3) 
     img = cv2.drawContours(img, dumby[1], reverse[areas[1]], (0, 0, 255), 3) 
+    moments = [cv2.moments(dumby[1][reverse[areas[0]]]), cv2.moments(dumby[1][reverse[areas[1]]])] #Moments of the two highest-area contours
+    cc0 = (int(moments[0]['m10']/moments[0]['m00']), int(moments[0]['m01']/moments[0]['m00']))
+    cc1 = (int(moments[1]['m10']/moments[1]['m00']), int(moments[1]['m01']/moments[1]['m00']))
+    cv2.circle(img, (cc0[0], cc0[1]), 12, (255, 0, 0), 3)
+    cv2.circle(img, (cc1[0], cc1[1]), 12, (255, 0, 0), 3)
+    x_avg = int((cc0[0] + cc1[0])/2)
+    y_avg = int((cc0[1] + cc1[1])/2) 
+    cv2.circle(img, (x_avg, y_avg), 14, (0, 255, 0), 4)
     cv2.imshow("the image", img)
     cv2.waitKey(0)  
 cv2.destroyAllWindows()

fa9b354bfe6c214ff4676dbf5d3a55a9ab0a45fb
FRC-283-PROG-2 <frcprogramming283.2@gmail.com>
2017-10-17 16:18:42 -0400

    Additional Development
    Ben
    Preivous changes commited

--- NapalmVision2017PostSCRIW/frc/pass_cm_double.py
+++ NapalmVision2017PostSCRIW/frc/pass_cm_double.py
@@ -9,21 +9,19 @@ vid2_path = "C:\\Users\\FRC_STANDARD_USER\\Desktop\\Python Vision Code\\Python V
 
 #Constants
 IP = "10.2.83.5"
-GREEN_LOWER = numpy.array([40, 0, 205])
-GREEN_UPPER = numpy.array([120, 100, 255])
+GREEN_LOWER = numpy.array([50, 0, 200])
+GREEN_UPPER = numpy.array([80, 30, 255])
 TABLE_NAME = "cv_data"
 
 #Main Code 
 camera = cv2.VideoCapture(0) #Stores the camera as a video source
+camera2 = cv2.VideoCapture(1) #Stores the other cam as a source
 print("Camera Opened: " + str(camera.isOpened()))
+print("Camera2 Opened: " + str(camera2.isOpened()))
 NetworkTables.setClientMode()
 NetworkTables.initialize(server=IP) #Target to retrieve table from
 table = NetworkTables.getTable(TABLE_NAME) #Save the cv_data table to a name.
 #The cv_data table is where all values will be written to
-jj, init_image = camera.read()
-cv2.imshow("cam", init_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 while True:
     print("=====")
     is_reading, img = camera.read() #is_reading is true if a image is grabbed. img is the captured image
@@ -65,9 +63,8 @@ while True:
                 cmx = (c0x + c1x)/2 #The image center of mass in x is the average
                 cmy = (c0y + c1y)/2 #The image center of mass in y is the average
                 height, width, channels = img.shape #Fetch the image properties
-                print(img.shape)
                 dx = (cmx) - (width - 1)/2 #Calculate the difference in the x between the center of mass and center of image
-                dy = (height - 1)/2 - (cmy)  #Calculate the difference in the y between the center of mass and the center of image. Signage correct
+                dy = (cmy) - (height - 1)/2 #Calculate the difference in the y between the center of mass and the center of image
                 table.putNumber("dx", dx) #Our final product is that vector
                 table.putNumber("dy", dy)
                 print("dx: " + str(dx))
@@ -81,9 +78,67 @@ while True:
         else:
             print("Cannot find at least 2 contours...")
         cv2.drawContours(img, contours, -1, (0, 0, 255), 3)
-        #cv2.imshow("incoming image", img)
-        #cv2.imshow("mask", mask)
-        #cv2.waitKey(0)
+        cv2.imshow("incoming image", img)
+        cv2.waitKey(0)
     else:
         print("Can't read camera image...")
+    
+    is_reading2, img2 = camera2.read() #is_reading is true if a image is grabbed. img is the captured image
+    #img = cv2.imread(img_path); is_reading = True #Comment in if using static image
+    table.putBoolean("is_reading2", is_reading2) #So the robot can detect the bug status of the program
+    if is_reading: #Only continue if we're successfully reading the image. (Prevents crashes)
+        hsv2 = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV) #Converts the image to Hue-Saturation-Lightness color coordinate scheme
+        mask2 = cv2.inRange(hsv2, GREEN_LOWER, GREEN_UPPER) #Returns a binary image of regions that are considered "green"
+        #A binary image has only two colors, black and white, which are used to signal properties about areas of the image
+        #In this case, WHITE regions represent areas that were considered "green"
+        blank2, contours2, other2 = cv2.findContours(mask2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) #Finds the curves that enclose "green" areas of the binary image
+        if (len(contours2) > 1):
+            #---Contours Sorting
+            #Explanation of Sorting: 
+                #A typical areas list would be [1500, 900, 1700, 1100]
+                #A typical reverse dict would be {1500: 0, 900:1, 1700:2, 1100:3} which as you can see just reverses the indexes
+                #A sorted areas list would be [1700, 1500, 1100, 900]
+                #So, by doing areas[0] we access the highest area, 1700
+                #And by doing reverse[1700], we get 0, the index of the highest area
+                #Hence, reverse[areas[0]] gives the ORIGINAL index of the highest area, 2
+                #reverse[areas[1]] is the second-highest, etc 
+            areas2 = [] #A list of contour areas
+            reverse2 = {} #A dictionary of contour indexes INDEXED BY contour areas
+            index2 = 0 #The index number of the contour currently being examined
+            for i2 in contours2: #For each contour
+                area_i2 = cv2.contourArea(i2) #Calculate the area for that contour
+                areas2 += [area_i2] #Add that onto the list of areas
+                reverse2.update({area_i2: index2}) #Append its area:index pair onto the reverse dict
+                index2 += 1
+            areas2 = sorted(areas2, key=float, reverse=True) #Sort the areas list so that the highest areas are in the lowest indices
+            #---End of Sorting
+            c0m2 = cv2.moments(contours2[reverse2[areas2[0]]]) #Moments (number-data) about the highest-area contour
+            c1m2 = cv2.moments(contours2[reverse2[areas2[1]]]) #Moments (number-data) about the second-highest-area contour
+            if c0m2['m00'] != 0 and c1m2['m00'] != 0: #Avoid division by 0
+                c0x2 = c0m2['m10']/c0m2['m00'] #Center of mass of contour 0 in the x
+                c0y2 = c0m2['m01']/c0m2['m00'] #Center of mass of contour 0 in the y
+                c1x2 = c1m2['m10']/c1m2['m00'] #Center of mass of contour 1 in the x
+                c1y2 = c1m2['m01']/c1m2['m00'] #Center of mass of contour 1 in the y
+                cmx2 = (c0x2 + c1x2)/2 #The image center of mass in x is the average
+                cmy2 = (c0y2 + c1y2)/2 #The image center of mass in y is the average
+                height2, width2, channels2 = img2.shape #Fetch the image properties
+                dx2 = (cmx2) - (width2 - 1)/2 #Calculate the difference in the x between the center of mass and center of image
+                dy2 = (cmy2) - (height2 - 1)/2 #Calculate the difference in the y between the center of mass and the center of image
+                table.putNumber("dx2", dx2) #Our final product is that vector
+                table.putNumber("dy2", dy2)
+                print("dx2: " + str(dx2))
+                print("dy2: " + str(dy2))
+                cv2.circle(img2, (int(cmx2), int(cmy2)), 14, (0, 255, 0), 4)
+            else:
+                print("2: A contour's m00 = 0!")
+                print("2: Updating dy = 0 and dx = 0")
+                table.putNumber("dx2", 0)
+                table.putNumber("dy2", 0)
+        else:
+            print("2: Cannot find at least 2 contours...")
+        cv2.drawContours(img2, contours2, -1, (0, 0, 255), 3)
+        cv2.imshow("incoming image2", img2)
+        cv2.waitKey(0)
+    else:
+        print("Can't read camera2 image...")
 camera.release #Frees the camera from the program
--- NapalmVision2017PostSCRIW/testing/color_test.py
+++ NapalmVision2017PostSCRIW/testing/color_test.py
@@ -6,18 +6,20 @@ photo_path = "C:\\Users\\FRC_STANDARD_USER\Desktop\\Python Vision Code\\Python V
 bright_path = "C:\\Users\\FRC_STANDARD_USER\\Desktop\\Python Vision Code\\Python Vision Repository\\NapalmVision2017PostSCRIW\\bright_test.png"
 sat_path = "C:\\Users\\FRC_STANDARD_USER\\Desktop\\Python Vision Code\\Python Vision Repository\\NapalmVision2017PostSCRIW\\sat_test.png"
 bd_path = "C:\\Users\\FRC_STANDARD_USER\\Desktop\\Python Vision Code\\Python Vision Repository\\NapalmVision2017PostSCRIW\\bd_test.png"
+bb_path = "C:\\Users\\FRC_STANDARD_USER\\Desktop\\Python Vision Code\\Python Vision Repository\\NapalmVision2017PostSCRIW\\bb_test.png"
+sd_path = "C:\\Users\\FRC_STANDARD_USER\\Desktop\\Python Vision Code\\Python Vision Repository\\NapalmVision2017PostSCRIW\\scoobydooby.png"
 
 
 #GREEN_LOWER = numpy.array([70, 170, 80])
 #GREEN_UPPER = numpy.array([140, 255, 255])
 
-img = cv2.imread(bd_path)
+img = cv2.imread(sd_path)
 img_copy = img.copy()
 img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-print("Bright zone values: " + str(img_hsv[70, 180])) 
-print("Metal zone values: " + str(img_hsv[20, 500])) 
-GREEN_LOWER = numpy.array([50, 0, 200])
-GREEN_UPPER = numpy.array([80, 30, 255])
+print("Primary zone values: " + str(img_hsv[80, 210])) 
+#print("Paper zone values: " + str(img_hsv[35, 330])) 
+GREEN_LOWER = numpy.array([50, 0, 235])
+GREEN_UPPER = numpy.array([100, 90, 255])
 mask = cv2.inRange(img_hsv, GREEN_LOWER, GREEN_UPPER)
 cv2.imshow("mask", mask)
 image, cons, hiers = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
